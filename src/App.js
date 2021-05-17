261c7d1dc261685203e7ea9149108b0383c85712
Esper95 <angeicjs95@gmail.com>
2021-05-17 15:23:00 +0900

    5/12 전화번호부. 수정 삭제 추가

--- src/App.js
+++ src/App.js
@@ -23,14 +23,34 @@ class App extends Component{
             information: information.concat({ id: this.id++, ...data })
         })
     }
+    handleRemove = (id) => {
+        const {information} =this.state;
+        this.setState({
+            information: information.filter(info => info.id !== id)
+        })
+    }
+    handleUpdate = (id, data) => {
+        const {information} = this.state;
+        this.setState({
+            information: information.map(
+                info => id === info.id
+                    ? {...info, ...data} // 새 객체를 만들어서 기존의 값과 전달받은 data를 덮어씀
+                    : info //기존의 값을 그대로 유지
+            )
+        })
+    }
     render() {
-        return (
-            <div>
-                <PhoneForm
-                    onCreate={this.handleCreate}
-                />
-                <PhoneInfoList data={this.state.information}/>
-            </div>
+        return(
+        <div>
+            <PhoneForm
+                onCreate={this.handleCreate}
+            />
+            <PhoneInfoList
+                data={this.state.information}
+                onRemove={this.handleRemove}
+                onUpdate={this.handleUpdate}
+            />
+        </div>
         );
     }
 }
